bdcb55a39ed93042f7d35854aa18bd63877716c4
Alumno <Alumno@PC08>
2019-10-17 10:44:50 -0500

    dfgvdg

---
+++ TiendadeVideojuegos/TiendadeVideojuegos/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data;
+
+
+namespace TiendadeVideojuegos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OleDbConnection con;
+        DataTable dt;
+        public MainWindow()
+        {
+            InitializeComponent();
+            con = new OleDbConnection();
+            con.ConnectionString = "Provider=Microsoft.Jet.Oledb.4.0; Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\TiendadeVideojuegos.mdb";
+            MostrarDatos();
+        }
+
+        private void MostrarDatos()
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Tienda";
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+            gvDatos.ItemsSource = dt.AsDataView();
+
+            if (dt.Rows.Count > 0)
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Hidden;
+                gvDatos.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Visible;
+                gvDatos.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void LimpiaTodo()
+        {
+            txtId.Text = "";
+            txtNombre.Text = "";
+            cbGenero.SelectedIndex = 0;
+            txtTelefono.Text = "";
+            txtNombredelVideojuego.Text = "";
+            btnNuevo.Content = "Nuevo";
+            txtId.IsEnabled = true;
+        }
+        //Editamos alumnos existentes
+        private void BtnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                txtId.Text = row["Id"].ToString();
+                txtNombre.Text = row["Nombre"].ToString();
+                cbGenero.Text = row["Genero"].ToString();
+                txtNombredelVideojuego.Text = row["Videojuego"].ToString();
+                txtTelefono.Text = row["Telefono"].ToString();
+                txtId.IsEnabled = false;
+                btnNuevo.Content = "Actualizar";
+            }
+            else
+            {
+                MessageBox.Show("Favor de seleccionar a un Comprador de la lista...");
+            }
+        }
+
+        //Eliminamos Alumnos
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+
+                OleDbCommand cmd = new OleDbCommand();
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "delete from Tienda where Id=" + row["Id"].ToString();
+                cmd.ExecuteNonQuery();
+                MostrarDatos();
+                MessageBox.Show("Comprador Eliminado correctamenta...");
+                LimpiaTodo();
+            }
+            else
+            {
+                MessageBox.Show("Favor de seleccionar a un Comprador de la lista...");
+            }
+        }
+        //Salimos de la aplicación
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        //Agregamos nuevos alumnos
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+
+            if (txtId.Text != "")
+            {
+                if (txtId.IsEnabled == true)
+                {
+                    if (cbGenero.Text != "Selecciona Genero")
+                    {
+                        cmd.CommandText = "insert into Tienda(Id,Nombre,Genero,Videojuego,Telefono) " +
+                            "Values(" + txtId.Text + ",'" + txtNombre.Text + "','" + cbGenero.Text + "'," + txtNombredelVideojuego.Text + "','" + txtTelefono.Text +  "')";
+                        cmd.ExecuteNonQuery();
+                        MostrarDatos();
+                        MessageBox.Show("Comprador agregado correctamente...");
+                        LimpiaTodo();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Favor de seleccionar el genero....");
+                    }
+                }
+                else
+                {
+                    cmd.CommandText = "update Tienda set Nombre='" + txtNombre.Text + "',Genero='" + cbGenero.Text + "'Videojuego=" + txtNombredelVideojuego.Text + "',Telefono=" + txtTelefono.Text
+                        + "' where Id=" + txtId.Text;
+                    cmd.ExecuteNonQuery();
+                    MostrarDatos();
+                    MessageBox.Show("Datos del Comprador Actualizados...");
+                    LimpiaTodo();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Favor de poner el ID de un Comprador.......");
+            }
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            LimpiaTodo();
+        }
+    }
+}
+
